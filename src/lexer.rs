b396ffeace71a76ce8893b2de3867ab339e12631
ilammy <a.lozovsky@gmail.com>
2015-10-01 21:47:37 +0300

    Scanning numbers
    Implemented scanning integers and floats, clarified some points about
    their lexical syntax inline. For example, in the end I have decided
    to allow trailing underscores in integers (like in 2___) as well as
    underscores around floating-point dot (like in 2__.__0). This allowed
    to simplify the scanner by *not* special-casing these sequences and
    treating _ just as another digit. After all, sane users will not abuse
    the separators, and I do not really care about insane users getting
    errors messages--yes, they *will* get them--with subpar descriptions.

--- src/lexer.rs
+++ src/lexer.rs
@@ -54,6 +54,12 @@ pub enum Token {
 
     /// Semicolon `;`
     Semicolon,
+
+    /// Integer literal
+    Integer,
+
+    /// Float literal
+    Float,
 }
 
 /// Span of a token
@@ -208,6 +214,11 @@ impl<'a> StringScanner<'a> {
         self.buf[self.pos..].chars().nth(0)
     }
 
+    /// Peek the character after the next without updating anything
+    fn peekpeek(&self) -> Option<char> {
+        self.buf[self.pos..].chars().nth(1)
+    }
+
     /// Scan the next token from the stream and set `tok`, `span`, `errors`
     fn next(&mut self) {
         if self.at_eof() {
@@ -266,6 +277,9 @@ impl<'a> StringScanner<'a> {
                         self.tok = Token::Colon;
                     }
                 }
+            }
+            '0'...'9' => {
+                self.scan_number();
             }
             _ => { panic!("unimplemented"); }
         }
@@ -365,6 +379,165 @@ impl<'a> StringScanner<'a> {
                  Please look for the missing block comment closure */");
 
             self.tok = Token::Comment;
+        }
+    }
+
+    /// Scan over any number
+    fn scan_number(&mut self) {
+        // Okay, first of all, numbers start with a decimal digit
+        assert!(StringScanner::is_digit(self.cur.unwrap(), 10));
+
+        // And then they have some digits as their integer part
+        let (base, digits) = match self.cur.unwrap() {
+            '0' => {
+                self.read();
+                match self.cur.unwrap_or('\0') {
+                    // "0b...", "0o...", "0x..." are non-decimal prefixes
+                    'b' => { self.read(); (2,  self.scan_digits( 2, 10)) }
+                    'o' => { self.read(); (8,  self.scan_digits( 8, 10)) }
+                    'x' => { self.read(); (16, self.scan_digits(16, 16)) }
+
+                    // Don't forget about that one zero we have already seen
+                    _ => { (10, self.scan_digits(10, 10) + 1) }
+                }
+            }
+            '1'...'9' => { (10, self.scan_digits(10, 10)) }
+
+            _ => { unreachable!(); }
+        };
+
+        // The integer part may contain no valid digits, like in "0o999" or "0x__"
+        if digits == 0 {
+            self.report.error(Span::new(self.start, self.prev_pos),
+                "The number contains no valid digits");
+        }
+
+        // According to what we have seen up to this point, it is integer
+        self.tok = Token::Integer;
+
+        // Though, a dot after an integer may mark a floating-point literal...
+        if self.cur == Some('.') {
+            let c = self.peek().unwrap_or('\0');
+
+            // ... if it is followed by another (decimal) integer
+            if StringScanner::is_digit(c, 10) || c == '_' {
+                self.read();
+                self.scan_float_fractional_part();
+                self.tok = Token::Float;
+            } else {
+                // Else it is just an integer followed by some dot. While dots can form identifiers
+                // (like '...'), type suffixes are defined as word-identifiers, which dots aren't.
+                // Thus we just go out with the suffixless integer we have already scanned over.
+                return;
+            }
+        }
+
+        // An 'e' may start either a floating-point exponent, or (technically) a type suffix.
+        // This is ambiguous, so we resolve it by greedily favoring the exponent treatment.
+        if self.cur == Some('e') || self.cur == Some('E') {
+            let c = self.peek().unwrap_or('\0');
+
+            if StringScanner::is_digit(c, 10) || c == '_' {
+                // An 'e' followed by a number is an exponent
+                self.read();
+                self.scan_float_exponent();
+                self.tok = Token::Float;
+            } else if c == '+' || c == '-' {
+                // An 'e' followed by a sign which is followed by a number is an exponent.
+                // If the sign is not followed by a number then we've seen just a suffix "e"
+                // with '+' or '-' being a part of some symbol-identifier that follows.
+                let c = self.peekpeek().unwrap_or('\0');
+
+                if StringScanner::is_digit(c, 10) || c == '_' {
+                    self.read();
+                    self.read();
+                    self.scan_float_exponent();
+                    self.tok = Token::Float;
+                }
+            } else {
+                // Not an exponent, just some (invalid) type suffix starting with 'e'.
+                // But we are in lexer and have no idea about is validity, so we go along.
+            }
+        }
+
+        // TODO: attempt to scan over a suffix
+
+        // Some late check for floating-point base. We do not support binary literals for floats.
+        if (self.tok == Token::Float) && (base != 10) {
+            self.report.error(Span::new(self.start, self.prev_pos),
+                "Float number must be decimal");
+        }
+    }
+
+    /// Scan over a sequence of digits
+    ///
+    /// Digits of `base` are valid. Digits of `allowed` base are scanned over,
+    /// but they are reported as erroneous if they are not fitting for `base`.
+    /// Also scans over any digit separators `_`.
+    ///
+    /// Returns a number of actual digits scanned (not counting the separators).
+    fn scan_digits(&mut self, base: u32, allowed: u32) -> usize {
+        assert!(base <= allowed);
+
+        let mut digits = 0;
+
+        while !self.at_eof() {
+            let c = self.cur.unwrap();
+
+            if c != '_' {
+                if !StringScanner::is_digit(c, base) {
+                    if StringScanner::is_digit(c, allowed) {
+                        self.report.error(Span::new(self.prev_pos, self.pos),
+                            "Unexpected digit"); // TODO: better message
+                    } else {
+                        break;
+                    }
+                }
+                digits += 1;
+            }
+
+            self.read();
+        }
+
+        return digits;
+    }
+
+    /// Check whether `c` is a valid digit of base `base`
+    fn is_digit(c: char, base: u32) -> bool {
+        match base {
+             2 => {  ('0' <= c) && (c <= '1') }
+             8 => {  ('0' <= c) && (c <= '7') }
+            10 => {  ('0' <= c) && (c <= '9') }
+            16 => { (('0' <= c) && (c <= '9')) ||
+                    (('a' <= c) && (c <= 'f')) ||
+                    (('A' <= c) && (c <= 'F')) }
+             _ => { panic!("invalid numeric base {}", base); }
+        }
+    }
+
+    /// Scan over a fractional part of a floating-point literal
+    fn scan_float_fractional_part(&mut self) {
+        assert!(StringScanner::is_digit(self.cur.unwrap(), 10) || self.cur == Some('_'));
+
+        let fractional_start = self.prev_pos;
+        let fractional_digits = self.scan_digits(10, 10);
+
+        if fractional_digits == 0 {
+            self.report.error(Span::new(fractional_start, self.prev_pos),
+                "The fractional part contains no valid digits");
+        }
+    }
+
+    /// Scan over an exponent part of a floating-point literal
+    fn scan_float_exponent(&mut self) {
+        assert!(StringScanner::is_digit(self.cur.unwrap(), 10) || self.cur == Some('_'));
+
+        let exponent_start = self.prev_pos;
+        let exponent_digits = self.scan_digits(10, 10);
+
+        if exponent_digits == 0 {
+            self.report.error(Span::new(exponent_start, self.prev_pos),
+                "The exponent contains no valid digits");
         }
     }
 }
@@ -545,6 +718,237 @@ mod tests {
             ScannerTestSlice(",",  Token::Comma),
         ], &[], &[]);
     }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Integers
+
+    #[test]
+    fn integer_basic_decimal() {
+        check(&[
+            ScannerTestSlice("0",     Token::Integer),
+            ScannerTestSlice(",",     Token::Comma),
+            ScannerTestSlice("1",     Token::Integer),
+            ScannerTestSlice(",",     Token::Comma),
+            ScannerTestSlice("00000", Token::Integer),
+            ScannerTestSlice(",",     Token::Comma),
+            ScannerTestSlice("00001", Token::Integer),
+            ScannerTestSlice(",",     Token::Comma),
+            ScannerTestSlice(" ",     Token::Whitespace),
+            ScannerTestSlice("42",    Token::Integer),
+            ScannerTestSlice(" ",     Token::Whitespace),
+            ScannerTestSlice("9137847823642354637463430", Token::Integer),
+            ScannerTestSlice("\n",    Token::Whitespace),
+            ScannerTestSlice("(",     Token::Lparen),
+            ScannerTestSlice("12345", Token::Integer),
+            ScannerTestSlice(")",     Token::Rparen),
+            ScannerTestSlice("67890", Token::Integer),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn integer_basic_nondecimal() {
+        check(&[
+            ScannerTestSlice("0b0110101010",    Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0o755",           Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0o32",            Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0o0",             Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0xDBE",           Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0x12345",         Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0xDeadBeef",      Token::Integer),
+            ScannerTestSlice(",",               Token::Comma),
+            ScannerTestSlice("0xAAAAAAAAAAAAA", Token::Integer),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn integer_separators() {
+        check(&[
+            ScannerTestSlice("0___",       Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("10_000",     Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("1_2_3_4_5",  Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("0x__5__",    Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("0b_0_1_1_0", Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("0o_7_7_7_",  Token::Integer),
+            ScannerTestSlice(" ",          Token::Whitespace),
+            ScannerTestSlice("0________0", Token::Integer),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn integer_unexpected_digit_range() {
+        check(&[
+            ScannerTestSlice("0b12345", Token::Integer),
+            ScannerTestSlice(",",       Token::Comma),
+            ScannerTestSlice("0o48_19", Token::Integer),
+            // There are no unexpected characters in decimal or hexadecimal literals.            // The first nondigit is either the start of a type suffix, or the first
+            // character of the next token.
+        ], &[ Span::new(3, 4), Span::new(4, 5), Span::new(5, 6), Span::new(6, 7),
+              Span::new(11, 12), Span::new(14, 15)], &[]);
+    }
+
+    #[test]
+    fn integer_unexpected_nondecimal_termination() {
+        check(&[
+            ScannerTestSlice("0x",  Token::Integer),
+            ScannerTestSlice(",",   Token::Comma),
+            ScannerTestSlice("0b",  Token::Integer),
+            ScannerTestSlice(",",   Token::Comma),
+            ScannerTestSlice("0o",  Token::Integer),
+            ScannerTestSlice(",",   Token::Comma),
+            ScannerTestSlice("0x_", Token::Integer),
+            ScannerTestSlice(",",   Token::Comma),
+            ScannerTestSlice("0b_", Token::Integer),
+            ScannerTestSlice(",",   Token::Comma),
+            ScannerTestSlice("0o_", Token::Integer),
+        ], &[ Span::new(0, 2), Span::new(3, 5), Span::new(6, 8), Span::new(9, 12),
+              Span::new(13, 16), Span::new(17, 20) ], &[]);
+    }
+
+    // TODO: combined errors
+
+    // TODO: type suffixes
+    //   correct suffixes
+    //   0g42 parses as 0 + g42, not as incorrect radix spec
+    //   0b22 is incorrect digits
+    //   0_b22 is (lexically) correct suffix
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Floats
+
+    #[test]
+    fn float_basic() {
+        check(&[
+            ScannerTestSlice("0.0",             Token::Float),
+            ScannerTestSlice(" ",               Token::Whitespace),
+            ScannerTestSlice("1.0",             Token::Float),
+            ScannerTestSlice(" ",               Token::Whitespace),
+            ScannerTestSlice("12313.123123121", Token::Float),
+            ScannerTestSlice(" ",               Token::Whitespace),
+            ScannerTestSlice("0.0000000005",    Token::Float),
+            ScannerTestSlice(" ",               Token::Whitespace),
+            ScannerTestSlice("0000000.1200000", Token::Float),
+            ScannerTestSlice(" ",               Token::Whitespace),
+            ScannerTestSlice("56.43453",        Token::Float),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn float_exponential_without_dot() {
+        check(&[
+            ScannerTestSlice("9e10",        Token::Float),
+            ScannerTestSlice(",",           Token::Comma),
+            ScannerTestSlice("1400e+12313", Token::Float),
+            ScannerTestSlice(",",           Token::Comma),
+            ScannerTestSlice("1400E-2323",  Token::Float),
+            ScannerTestSlice("//\n",        Token::Comment),
+            ScannerTestSlice("0e+0",        Token::Float),
+            ScannerTestSlice(",",           Token::Comma),
+            ScannerTestSlice("0001E1000",   Token::Float),
+            ScannerTestSlice(",",           Token::Comma),
+            ScannerTestSlice("0e-0",        Token::Float),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn float_exponential_with_dot() {
+        check(&[
+            ScannerTestSlice("2.0E10",           Token::Float),
+            ScannerTestSlice(")",                Token::Rparen),
+            ScannerTestSlice("0.00000e0",        Token::Float),
+            ScannerTestSlice("[",                Token::Lbrack),
+            ScannerTestSlice("123.456e+789",     Token::Float),
+            ScannerTestSlice("}",                Token::Rbrace),
+            ScannerTestSlice("11.11E-11",        Token::Float),
+            ScannerTestSlice("{",                Token::Lbrace),
+            ScannerTestSlice("9363.83929e00434", Token::Float),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn float_extreme() {
+        // Just making sure that scanner does not care about semantics
+        check(&[
+            ScannerTestSlice("999999999999999999999999.999999999999999999", Token::Float),
+            ScannerTestSlice("\r\n",                                        Token::Whitespace),
+            ScannerTestSlice("0.0000000000000000000e-99999999999999999999", Token::Float),
+            ScannerTestSlice("\r\n",                                        Token::Whitespace),
+            ScannerTestSlice("1.2345678901234567890E+12345678901234567890", Token::Float),
+            ScannerTestSlice("\r\n",                                        Token::Whitespace),
+            ScannerTestSlice("12345678.901234567890E123456789012345678901", Token::Float),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn float_separators() {
+        check(&[
+            ScannerTestSlice("10_000.000_001", Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("0______0.5",     Token::Float),
+            ScannerTestSlice(":",              Token::Colon),
+            ScannerTestSlice("1_._2",          Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("3._4_",          Token::Float),
+            ScannerTestSlice(":",              Token::Colon),
+            ScannerTestSlice("7___e54",        Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("3_._1_E_4",      Token::Float),
+            ScannerTestSlice(":",              Token::Colon),
+            ScannerTestSlice("0.0e+___4",      Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("0._0E-__5__",    Token::Float),
+        ], &[], &[]);
+    }
+
+    #[test]
+    fn float_deny_radix_spec() {
+        check(&[
+            ScannerTestSlice("0b1e101101",         Token::Float),
+            ScannerTestSlice("          ",         Token::Whitespace),
+            ScannerTestSlice("0b01011011.0110101", Token::Float),
+            ScannerTestSlice("/*      */",         Token::Comment),
+            ScannerTestSlice("0o7.7",              Token::Float),
+            ScannerTestSlice("          ",         Token::Whitespace),
+            ScannerTestSlice("0o77e10",            Token::Float),
+            ScannerTestSlice("          ",         Token::Whitespace),
+            ScannerTestSlice("0x00.0E7",           Token::Float),
+            ScannerTestSlice("          ",         Token::Whitespace),
+            ScannerTestSlice("0x5.1",              Token::Float),
+            ScannerTestSlice("          ",         Token::Whitespace),
+            ScannerTestSlice("0o5e+3",             Token::Float),
+        ], &[ Span::new(0, 10), Span::new(20, 38), Span::new(48, 53), Span::new(63, 70),
+              Span::new(80, 88), Span::new(98, 103), Span::new(113, 119) ], &[]);
+    }
+
+    #[test]
+    fn float_missing_numbers() {
+        check(&[
+            ScannerTestSlice("5._____",        Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("0._",            Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("1e___",          Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("5.0E+___",       Token::Float),
+            ScannerTestSlice(",",              Token::Comma),
+            ScannerTestSlice("10e-___",        Token::Float),
+        ], &[ Span::new(2, 7), Span::new(10, 11), Span::new(14, 17),
+              Span::new(23, 26), Span::new(31, 34) ], &[]);
+    }
+
+    // TODO: combined errors
+
+    // TODO: type suffixes
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // Test helpers
